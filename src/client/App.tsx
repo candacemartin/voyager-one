1881bf8cbbfb0572ad424d66233f6739a667ecac
CE Martin <16669684+candacemartin@users.noreply.github.com>
2023-05-16 12:01:34 -0400

    adds signup

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
 // import Button from '@mui/material/Button';
-import NewCard from './components/Card';
+import SignUp from './components/SignUp';
 
 
 const App = () => {
@@ -8,8 +8,7 @@ const App = () => {
     return (
         <div>
             <h1>check out my shroom react app lol this is hot</h1>
-            <NewCard />
-            {/* <Button variant="outlined">hi there</Button> */}
+            <SignUp />
         </div>
     )
 };
