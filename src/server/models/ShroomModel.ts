0d566840f705b08520c810e1fa0b496693bae72b
carmen <72165737+BadWithNames@users.noreply.github.com>
2023-05-17 14:16:25 -0400

    set up UserModel&reverted back to ES module syntax

--- src/server/models/ShroomModel.ts
+++ src/server/models/ShroomModel.ts
@@ -1,26 +1,22 @@
-// import mongoose, { Schema, Document } from 'mongoose';
-
-const mongoose = require('mongoose')
+import mongoose, { Schema, Document } from 'mongoose';
 
 //define the interface for shroom document
 interface Shroom extends Document {
-    name: string;
-    description: string;
-    type: string;
-    createdAt: Date;
-    updatedAt: Date;
-};
+  name: string;
+  description: string;
+  type: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
 
 //define schema
-const shroomSchema = new mongoose.Schema({
-    name: {type: String, required: true },
-    description: { type: String },
-    type: { type: String },
-    createdAt: { type: Date, default: Date.now },
-    updatedAt: { type: Date, default: Date.now },
+const shroomSchema: Schema = new mongoose.Schema({
+  name: { type: String, required: true },
+  description: { type: String },
+  type: { type: String },
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now },
 });
 
 //define model
-const Shroom = mongoose.model('Shroom', shroomSchema);
-
-module.exports = Shroom
+export default mongoose.model<Shroom>('Shroom', shroomSchema);
--- src/server/models/UserModel.ts
+++ src/server/models/UserModel.ts
@@ -1,28 +1,24 @@
-// import mongoose, { Schema, Document } from 'mongoose';
-
-const monGoose = require('mongoose')
+import mongoose, { Schema, Document } from 'mongoose';
 
 //define the interface for user document
 interface User extends Document {
-    firstName: string;
-    lastName: string;
-    email: string;
-    password: string;
-    createdAt: Date;
-    updatedAt: Date;
-};
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
 
 //define schema
-const userSchema = new monGoose.Schema({
-    firstName: {type: String, required: true },
-    lastName: {type: String, required: true },
-    email: {type: String, required: true },
-    password: {type: String, required: true },
-    createdAt: { type: Date, default: Date.now },
-    updatedAt: { type: Date, default: Date.now },
+const userSchema: Schema = new mongoose.Schema({
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now },
 });
 
 //define model
-const User = monGoose.model('User', userSchema);
-
-module.exports = User
+export default mongoose.model<User>('User', userSchema);
