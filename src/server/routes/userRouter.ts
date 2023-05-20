5c8beadfcd87e63ae1724fa951f7e4bb81f58661
carmen <72165737+BadWithNames@users.noreply.github.com>
2023-05-20 15:43:50 -0400

    added typing

--- src/server/routes/userRouter.ts
+++ src/server/routes/userRouter.ts
@@ -1,9 +1,9 @@
-import { Router } from 'express';
+import { Router, Response } from 'express';
 import userController from '../controllers/userController';
 
 const router: Router = Router();
 
-router.post('/register', userController.registerUser, (_, res) => {
+router.post('/register', userController.registerUser, (_, res: Response) => {
   console.log('inside userRouter.post');
   return res.send(res.locals.user);
 });
