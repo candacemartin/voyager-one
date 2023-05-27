d1f4423a3b99dc34d5ae95f1950d4f5024380fb9
carmen <72165737+BadWithNames@users.noreply.github.com>
2023-05-27 17:53:27 -0400

    add property to GrowModel

--- server/models/GrowModel.ts
+++ server/models/GrowModel.ts
@@ -1,7 +1,9 @@
 import mongoose, { Schema, Document } from 'mongoose';
+import UserModel from './UserModel';
 
 //define the interface for grow document
 interface Grow extends Document {
+  created_by: string;
   mushroom_type: string;
   container: string;
   substrate: string;
@@ -13,13 +15,14 @@ interface Grow extends Document {
 
 //define schema
 const growSchema: Schema = new mongoose.Schema({
-  mushroom_type: {type: String, required: true},
-  container: {type: String, required: true },
-  substrate: {type: String, required: true },
-  inoculation_method: {type: String, required: true },
-  start_date: {type: Date, default: Date.now },
-  createdAt: {type: Date, default: Date.now },
-  updatedAt: {type: Date, default: Date.now },
+  created_by: { type: Schema.Types.ObjectId, ref: UserModel, required: true },
+  mushroom_type: { type: String, required: true },
+  container: { type: String, required: true },
+  substrate: { type: String, required: true },
+  inoculation_method: { type: String, required: true },
+  start_date: { type: Date, default: Date.now },
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now },
 });
 
 //define model
