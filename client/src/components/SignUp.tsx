da2d15f683f1e0864679b8910c1735d5d2aead9d
BadWithNames <72165737+BadWithNames@users.noreply.github.com>
2023-06-01 21:46:46 -0400

    modify SignUp component

--- client/src/components/SignUp.tsx
+++ client/src/components/SignUp.tsx
@@ -1,187 +1,115 @@
-import * as React from 'react';
 import { useState } from 'react';
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
-import Link from '@mui/material/Link';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
 
-interface User {
-  firstName: string;
-  lastName: string;
-  email: string;
-  password: string;
-}
+// function Copyright(props: any) {
+//   return (
+//     <Typography
+//       variant='body2'
+//       color='text.secondary'
+//       align='center'
+//       {...props}
+//     >
+//       {'copyright © '}
+//       <Link color='inherit' href='https://mui.com/'>
+//         your website
+//       </Link>{' '}
+//       {new Date().getFullYear()}
+//       {'.'}
+//     </Typography>
+//   );
+// }
 
-function Copyright(props: any) {
-  return (
-    <Typography
-      variant='body2'
-      color='text.secondary'
-      align='center'
-      {...props}
-    >
-      {'copyright © '}
-      <Link color='inherit' href='https://mui.com/'>
-        your website
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
-}
-
-function GoogleAuth(props: any) {
-  return (
-    <div>
-      <a href="/auth/google">Login with Google</a>
-    </div>
-  )
-}
-
-const theme = createTheme();
+// function GoogleAuth(props: any) {
+//   return (
+//     <div>
+//       <a href="/auth/google">Login with Google</a>
+//     </div>
+//   )
+// }
 
 export default function SignUp() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [firstName, setFirstName] = useState('');
-  const [lastName, setLastName] = useState('');
+  const [signUpToEmails, setSignUpToEmails] = useState(false);
 
   const handleSubmit = async (
     event: React.MouseEvent<HTMLButtonElement, MouseEvent>,
   ) => {
     event.preventDefault();
-    const user: User = {
-      firstName,
-      lastName,
-      email,
-      password,
-    };
-    console.log(user);
-    await fetch('/api/user/register', {
-      method: 'post',
-      headers: new Headers({ 'Content-Type': 'application/json' }),
-      body: JSON.stringify(user),
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        return data;
-      })
-      .catch((err) => {
-        return `inside handle catch ${err}`;
-      });
   };
 
+  const handleSignUpToEmails = () => setSignUpToEmails(!signUpToEmails);
+
   return (
-    <ThemeProvider theme={theme}>
-      <Container component='main' maxWidth='xs'>
-        <CssBaseline />
-        <Box
-          sx={{
-            marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-          }}
-        >
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component='h1' variant='h5'>
-            sign up
-          </Typography>
-          <Box component='form' noValidate sx={{ mt: 3 }}>
-            <Grid container spacing={2}>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  autoComplete='given-name'
-                  name='firstName'
-                  required
-                  fullWidth
-                  id='firstName'
-                  label='first name'
-                  value={firstName}
-                  onChange={(e) => setFirstName(e.target.value)}
-                  autoFocus
-                />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  required
-                  fullWidth
-                  id='lastName'
-                  label='last name'
-                  name='lastName'
-                  value={lastName}
-                  onChange={(e) => setLastName(e.target.value)}
-                  autoComplete='family-name'
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  id='email'
-                  label='email address'
-                  name='email'
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                  autoComplete='email'
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  name='password'
-                  label='password'
-                  type='password'
-                  id='password'
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                  autoComplete='new-password'
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <FormControlLabel
-                  control={
-                    <Checkbox value='allowExtraEmails' color='primary' />
-                  }
-                  label='click here if you want to receive inspiration, marketing promotions and updates via email :)'
-                />
-              </Grid>
-            </Grid>
-            <Button
-              type='submit'
+    <Box
+      sx={{
+        marginTop: 8,
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+      }}
+    >
+      <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+        <LockOutlinedIcon />
+      </Avatar>
+      <Typography component='h1' variant='h5'>
+        sign up
+      </Typography>
+      <Box component='form' noValidate sx={{ mt: 3 }}>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <TextField
+              required
               fullWidth
-              variant='contained'
-              sx={{ mt: 3, mb: 2 }}
-              onClick={(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) =>
-                handleSubmit(e)
-              }
-            >
-              sign up
-            </Button>
-            <Grid container justifyContent='flex-end'>
-              <Grid item>
-                <Link href='#' variant='body2'>
-                  already have an account? sign in
-                </Link>
-              </Grid>
-            </Grid>
-          </Box>
-        </Box>
-        <Copyright sx={{ mt: 5 }} />
-        <GoogleAuth />
-      </Container>
-    </ThemeProvider>
+              id='email'
+              label='email address'
+              name='email'
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              autoComplete='email'
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              required
+              fullWidth
+              name='password'
+              label='password'
+              type='password'
+              id='password'
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              autoComplete='new-password'
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <FormControlLabel
+              onClick={() => handleSignUpToEmails()}
+              control={<Checkbox value='allowExtraEmails' color='primary' />}
+              label='click here if you want to receive inspiration, marketing promotions and updates via email :)'
+            />
+          </Grid>
+        </Grid>
+        <Button
+          type='submit'
+          fullWidth
+          variant='contained'
+          sx={{ mt: 3, mb: 2 }}
+          onClick={(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) =>
+            handleSubmit(e)
+          }
+        >
+          sign up
+        </Button>
+      </Box>
+    </Box>
   );
 }
