597723774676df9bfc74f72d69b466601b3d2910
BadWithNames <72165737+BadWithNames@users.noreply.github.com>
2023-06-02 17:38:06 -0400

    update AuthBar to add icons

--- client/src/components/AuthBar.tsx
+++ client/src/components/AuthBar.tsx
@@ -3,6 +3,9 @@ import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
 
 export default function AuthBar() {
   return (
@@ -12,6 +15,12 @@ export default function AuthBar() {
           <Typography variant='h6' component='div' sx={{ flexGrow: 1 }}>
             MycoTech
           </Typography>
+          <IconButton>
+            <LinkedInIcon />
+          </IconButton>
+          <IconButton>
+            <GitHubIcon />
+          </IconButton>
           <Button color='inherit'>Login</Button>
         </Toolbar>
       </AppBar>
