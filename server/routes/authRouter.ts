ba4c725f64d2b4d6bd475fad2623f73d48f01c14
BadWithNames <72165737+BadWithNames@users.noreply.github.com>
2023-06-05 15:11:24 -0400

    update authRouter

--- server/routes/authRouter.ts
+++ server/routes/authRouter.ts
@@ -1,16 +1,26 @@
-import { Router, Response } from 'express';
+import Router, { Router as IRouter, Request, Response } from 'express';
 
-const router: Router = Router();
-const passport = require('../passport');
+import passport from '../config/passport';
+import authController from '../controllers/authController';
 
-router.get('/v1/auth/google', 
-    passport.authenticate('google', { 
-        session: false, 
-        scope: ['profile', 'email'] 
-    }));
+const router: IRouter = Router();
+
+router.post('/signup', authController.signUp, (req: Request, res: Response) =>
+  res.send({ message: 'successful signup', user: req.user }),
+);
+
+router.get(
+  '/login',
+  passport.authenticate('local'),
+  (req: Request, res: Response) => res.send({ message: 'successful login' }),
+);
+
+// tells passport to intercept all get reqs to /auth/google endpoint and handle auth
+// router.get(
+//   '/google',
+//   passport.authenticate('google', { scope: ['profile'] }), // scope tells passport what info we want from google
+//   (req: Request, res: Response) =>
+//     res.status(200).send('successful auth with google'),
+// );
 
 export default router;
-
-
-
-
