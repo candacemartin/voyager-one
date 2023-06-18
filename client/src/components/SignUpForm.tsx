ee18e60abe85ef92b571f30e48a4708684175294
BadWithNames <72165737+BadWithNames@users.noreply.github.com>
2023-06-17 21:54:42 -0400

    added navigation

--- client/src/components/SignUpForm.tsx
+++ client/src/components/SignUpForm.tsx
@@ -1,11 +1,13 @@
 import { useState } from 'react';
 import Box from '@mui/material/Box';
+import { useNavigate } from 'react-router-dom';
 
 import SignUpInput from './SignUpInput';
 import SignUpButton from './SignUpButton';
 import SignUpEmails from './SignUpEmails';
 
 export default function SignUpForm(): React.JSX.Element {
+  const navigate = useNavigate();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [subscribedToEmails, setSubscribedToEmails] = useState(false);
@@ -23,8 +25,12 @@ export default function SignUpForm(): React.JSX.Element {
         subscribedToEmails,
       }),
     })
-      .then((res) => res.json())
-      .then((data) => console.log(data))
+      .then(() => {
+        setEmail('');
+        setPassword('');
+        setSubscribedToEmails(false);
+        return navigate('/dashboard');
+      })
       .catch((err) => console.error(err));
   };
 
