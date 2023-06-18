f6a6cc3b69bd1be244fc37023c182cb2400460fc
BadWithNames <72165737+BadWithNames@users.noreply.github.com>
2023-06-17 22:01:46 -0400

    update openai config

--- server/config/openai.config.ts
+++ server/config/openai.config.ts
@@ -1,21 +1,9 @@
-const { Configuration, OpenAIApi } = require("openai");
-
-// const configuration = new Configuration({
-//   apiKey: process.env.OPENAI_API_KEY,
-// });
-
-// const openai = new OpenAIApi(configuration);
+import { Configuration, OpenAIApi } from 'openai';
 
 const configuration = new Configuration({
-  apiKey: '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',
+  apiKey: process.env.OPENAI_API_KEY,
 });
+
 const openai = new OpenAIApi(configuration);
-
-// openai.createCompletion({
-//   model: "text-davinci-003",
-//   prompt: "tell me the color of grass",
-// }).then((res: any) => console.log(res.data.choices)).catch((err: Error) => console.log(err));
-
-
 
 export default openai;
